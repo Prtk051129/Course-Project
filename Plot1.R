1c81a52e0c8cf1781cb0baad35329e4eabc24722
Prateek Agrawal <prateekhustler29@gmail.com>
2019-01-20 22:52:51 +0530

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,27 @@
+library(lubridate)
+library(tidyverse)
+library(magrittr)
+
+#path to the file
+path<-file.choose()
+
+#read the txt file
+myData<-read.table(file=path,header=T,sep=";",stringsAsFactors = F,check.names = F)
+
+#View complete data in R
+View(myData)
+
+#Change the data format of Date column to yyyy-mm-dd
+myData$Date<-dmy(myData$Date)
+
+#Filter data for 1st & 2nd Feb 2007
+df<-myData[myData$Date=="2007-02-01" | myData$Date=="2007-02-02",]
+unique(df$Date)
+View(df)
+
+
+class(df$Global_active_power)
+df$Global_active_power<-as.numeric(df$Global_active_power)
+
+#Plot
+hist(df$Global_active_power, col="red")
